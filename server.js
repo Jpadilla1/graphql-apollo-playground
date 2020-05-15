33740a0ed67ea32c049050136393c4981f8068b6
Jose Padilla <jpadilla@lyft.com>
2020-05-14 18:46:19 -0700

    set up graphiql

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlExpress = require('apollo-server-express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 
 const myGraphQLSchema = ``;
 const PORT = 8080;
 
 const app = express();
 
-// bodyParser is needed just for POST.
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.use(
+  "/graphiql",
+  graphiqlExpress({
+    endpointURL: "/graphql",
+  })
+);
 
+// bodyParser is needed just for POST.
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({ schema: myGraphQLSchema })
+);
 
 app.listen(PORT);
